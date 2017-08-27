6c494c10f03454545fe8bd814c85b29a16b1f7d7
Anthony Ribeiro <anthonyribeiro1910@gmail.com>
2017-08-27 19:57:56 -0300

    Primeiro Projeto - Hello World

---
+++ MyApp/MyApp/HelloWorld.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp
+{
+    class HelloWorld
+    {
+        /* Hello World simples
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        */
+
+        /* Hello World identificando valores informados
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("You entered the following {0} command line arguments:", args.Length);
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine("{0}", args[i]);
+            }
+        }
+        */
+
+        // Hello World com retorno int
+        static int Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            return 42;
+        }
+
+    }
+}
