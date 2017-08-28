6cf371afb4a96f2aeae60d60cc0d4809ff8ab187
Anthony Ribeiro <anthonyribeiro1910@gmail.com>
2017-08-27 21:48:27 -0300

    Entendendo Referências de Classes e métodos estáticos

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int x = 3;
+            int y = 4;
+            int result = MyClassLibrary.MyMath.AddNumbers(x, y);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ ConsoleApp1/MyClassLibrary/MyMath.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyClassLibrary
+{
+    public class MyMath
+    {
+        public static int AddNumbers(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
