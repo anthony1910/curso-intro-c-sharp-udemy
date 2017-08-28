1a3e3bc6a72b2a5816f8ce28cd921a1eaf104c9a
Anthony Ribeiro <anthonyribeiro1910@gmail.com>
2017-08-27 21:06:54 -0300

    Primeiro arquivo C# separado

--- MyApp/MyApp/HelloWorld.cs
+++ MyApp/MyApp/HelloWorld.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
+/* com namespace
 namespace MyApp
 {
     class HelloWorld
@@ -34,5 +31,15 @@ namespace MyApp
             return 42;
         }
 
+    }
+}
+*/
+
+// sem namespace
+class HelloWorld
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello World!");
     }
 }
