0ac6a0ed32d143e841d095f4244c63dc4f50e65b
Anthony Ribeiro <anthonyribeiro1910@gmail.com>
2017-09-08 14:55:44 -0300

    Terminando Aula 10

---
+++ Types/Program/Program/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyNameSpace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = a + 2; // OK no problem
+
+            bool test = true;
+
+            //int c = a + test; // Error. Bool and int não pode
+
+        }
+    }
+}
