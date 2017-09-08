ae4e55d9c47eb8c645906becd2dc90476a37228c
Anthony Ribeiro <anthonyribeiro1910@gmail.com>
2017-09-08 18:42:54 -0300

    Terminando Aula 11

---
+++ Types/Variables.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Variables
+{
+	static void Main(string[] args)
+    {
+
+        int limit = 3;
+
+        int[] source = { 0, 1, 2, 3, 4, 5 };
+
+        Console.WriteLine(limit);
+        Console.WriteLine(source);
+        Console.ReadKey();
+
+    }
+}
