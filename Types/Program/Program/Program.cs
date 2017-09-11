dea5e4ff59c4cce54c2f1030dc6c8f6b6ce5514a
Anthony Ribeiro <anthonyribeiro1910@gmail.com>
2017-09-11 13:05:41 -0300

    Terminando Aula 13

--- Types/Program/Program/Program.cs
+++ Types/Program/Program/Program.cs
@@ -4,6 +4,7 @@ namespace MyNameSpace
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
             int a = 5;
@@ -14,5 +15,6 @@ namespace MyNameSpace
             //int c = a + test; // Error. Bool and int não pode
 
         }
+
     }
 }
---
+++ Types/Variables2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Variable
+{
+    static void Main(string[] args)
+    {
+        string result = GetName(-1);
+        Console.WriteLine(result);
+        Console.ReadKey();
+    }
+
+    public static string GetName(int ID)
+    {
+        if (ID >= 0 && ID < names.Length)
+            return names[ID];
+        else
+            return String.Empty;
+    }
+
+    private static string[] names = { "Rafael", "João", "Glaucio" };
+
+}
