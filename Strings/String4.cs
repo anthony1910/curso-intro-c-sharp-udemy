06db0c92d0d3b07ab042a3ef4f8728872ac30914
Anthony Ribeiro <anthonyribeiro1910@gmail.com>
2017-09-25 16:22:46 -0300

    Terminando Aula 27

---
+++ Strings/String4.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Class4
+{
+	public Class4()
+	{
+        // Substring e Replace
+        string s1 = "Visual C# Express";
+        Console.WriteLine(s1.Substring(7, 2));
+
+        Console.WriteLine(s1.Replace("C#", "Basic"));
+
+        Console.WriteLine(s1.IndexOf("C"));
+        Console.ReadKey();
+    }
+}
