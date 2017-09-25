b9185218548161db28d69da5198c1e6d4f53f11c
Anthony Ribeiro <anthonyribeiro1910@gmail.com>
2017-09-25 15:22:30 -0300

    Terminando Aula 25

---
+++ Strings/String2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Class2
+{
+	public Class2()
+	{
+        char ch = 'A';
+        Console.WriteLine(ch);
+        Console.ReadKey();
+
+        char ch = '\'';
+        Console.WriteLine(ch);
+        Console.ReadKey();
+
+        string str = "Hello\nWorld"; // quebra de linha
+        Console.WriteLine(str);
+        Console.ReadKey();
+
+        Console.WriteLine("Hello\bWorld"); // backspace
+        Console.ReadKey();
+
+        Console.WriteLine("Hello\tWorld"); // tab
+        Console.ReadKey();
+    }
+}
